0879e5601a8ae4948faf402824c6bc074105c366
Matthias Braun <matze@braunis.de>
2014-08-30 16:27:46 +0200

    improve BUY/SELL analysis

--- gc2ledger.py
+++ gc2ledger.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 import sqlite3
 import sys
+import codecs
 from datetime import datetime
 
-out = sys.stdout
+out = codecs.getwriter('UTF-8')(sys.stdout)
 
 conn = sqlite3.connect('test.db')
 
@@ -18,13 +19,20 @@ class Account(object):
 		self.parent = None
 		self.name = ""
 		self.splits = []
+		self.type = None
 		self.dummy = True
+	
+	def __str__(self):
+		return self.name
 
 class Commodity(object):
 	def __init__(self):
 		self.fullname = ""
 		self.namespace = ""
 		self.dummy = True
+
+	def __str__(self):
+		return self.mnemonic
 
 class Transaction(object):
 	def __init__(self):
@@ -76,6 +84,7 @@ for row in c.execute('SELECT guid, name, account_type, commodity_guid, commodity
 	acc.parent.childs.append(acc)
 	acc.description = description
 	acc.commodity = get_commodity(commodity_guid)
+	acc.type = account_type
 	acc.dummy = acc.commodity.dummy
 
 def parse_time(time):
@@ -97,10 +106,12 @@ for row in c.execute('SELECT guid, tx_guid, account_guid, value_num, value_denom
 	split.transaction.splits.append(split)
 	split.account = get_account(account_guid)
 	split.account.splits.append(split)
-	split.value_num = value_num
-	split.value_denom = value_denom
-	split.quantity_num = quantity_num
-	split.quantity_denom = quantity_denom
+	split.value_num = int(value_num)
+	split.value_denom = int(value_denom)
+	split.value = float(value_num)/float(value_denom)
+	split.quantity_num = int(quantity_num)
+	split.quantity_denom = int(quantity_denom)
+	split.quantity = float(quantity_num)/float(quantity_denom)
 
 def full_acc_name(acc, maxdepth=1000):
 	if acc.parent is None or maxdepth == 0:
@@ -109,20 +120,128 @@ def full_acc_name(acc, maxdepth=1000):
 	result += ":"+acc.name
 	return result
 
-#for acc in accounts.values():
-#	if acc.name == "":
-#		continue
-#	name = full_acc_name(acc)
-#	out.write("%s - %s (%s)\n" % (name, acc.description, acc.commodity.fullname))
+# Calculate dividend amounts per account
+def calc_dividends(acc):
+	# Collect transactions involving currency and no number on this account
+	sum_value = 0.0
+	sum_fees = 0.0
+	sum_dividends = 0.0
+	count = 0
+	for split in acc.splits:
+		trans = split.transaction
+		if split.quantity != 0:
+			continue
+		value = 0.0
+		fees = 0.0
+		dividends = 0.0
+		for ssplit in trans.splits:
+			if ssplit is split:
+				continue
+			#date = trans.post_date.strftime("%d.%m.%Y")
+			#out.write("   %s %-30s   value %.2f quantity %.2f\n" %
+			#		  (date, trans.description, ssplit.value, ssplit.quantity))
+			acctype = ssplit.account.type
+			if acctype == "EXPENSE":
+				fees += abs(ssplit.value)
+			elif acctype == "BANK":
+				value += abs(ssplit.value)
+			elif acctype == "INCOME":
+				dividends += abs(ssplit.value)
+			else:
+				out.write("UNKNOWN TYPE: %s (acc %s)\n" % (acctype, acc))
+				date = trans.post_date.strftime("%d.%m.%Y")
+				out.write("\t%s %-30s   value %.2f quantity %.2f\n" %
+						  (date, trans.description, ssplit.value, ssplit.quantity))
+				assert False
+		count += 1
+		sum_value += value
+		sum_fees += fees
+		sum_dividends += dividends
+		date = trans.post_date.strftime("%d.%m.%Y")
+		out.write("\t%s DIV   %7.2f %s fees %7.2f\n" % (date, value, trans.currency, fees))
+	
+	assert abs(sum_value+sum_fees - sum_dividends) < .0001
+	return (count, sum_value, sum_fees)
+
+def calc_realized_wins(acc):
+	for split in acc.splits:
+		trans = split.transaction
+		# quantity == 0 is probably a dividend, we don't care here
+		if split.quantity == 0:
+			continue
+		date = trans.post_date.strftime("%d.%m.%Y")
+		curr = trans.currency
+		is_buy = split.quantity > 0
+
+		# Classify remaining splits into taxes+fees and real costs
+		fees=0.0
+		costs=0.0
+		atype = "BUY " if is_buy else "SELL"
+		ignore_trans = False
+		for ssplit in trans.splits:
+			if ssplit is split or ssplit.value == 0:
+				continue
+			acctype = ssplit.account.type
+			if acctype == "EXPENSE":
+				fees += abs(ssplit.value)
+			elif acctype == "BANK" or acctype == "ASSET":
+				costs += abs(ssplit.value)
+			elif acctype == "STOCK":
+				# moved to different depot? or a stock split?
+				if ssplit.account == acc:
+					# probably a split, ignore
+					ignore_trans = True
+				else:
+					# moved to/from different depot, handle like a SELL
+					assert costs == 0
+					atype = "MIN " if is_buy else "MOUT"
+					costs += abs(ssplit.value)
+			else:
+				out.write("UNKNOWN TYPE: %s (acc %s)\n" % (acctype, acc))
+				date = trans.post_date.strftime("%d.%m.%Y")
+				out.write("\t%s %-30s   value %.2f quantity %.2f\n" %
+						  (date, trans.description, ssplit.value, ssplit.quantity))
+				assert False
+
+		if ignore_trans:
+			continue
+
+		quantity = abs(split.quantity)
+		out.write("\t%s %s %7s shares costs %9.2f %s fees %7.2f\n" % (
+		          date, atype, quantity, costs, curr, fees))
+
+		#name = full_acc_name(ssplit.account, 2)
+		#out.write("     acc %s value %.2f quantity %.2f\n" % (name, ssplit.value, ssplit.quantity))
 
 # Select all stock accounts
-stockaccounts = []
+gdiv_value = 0.0
+gdiv_fees = 0.0
+gdiv_count = 0
 for acc in accounts.values():
-	if acc.dummy or acc.commodity.namespace == "CURRENCY":
+	if acc.type != "STOCK":
+		#out.write("IGNORE %s: %-40s\n" % (acc.type, full_acc_name(acc, 3)))
 		continue
-	stockaccounts.append(acc)
 	name = full_acc_name(acc, 3)
-	out.write("%-35s - %s (%s)\n" % (name, acc.description, acc.commodity.fullname))
+	out.write("%-40s\n" % (name,))
+
+	div_count, div_value, div_fees = calc_dividends(acc)
+	acc.div_value = div_value
+	acc.div_count = div_count
+	acc.div_fees = div_fees
+	gdiv_value += div_value
+	gdiv_count += div_count
+	gdiv_fees += div_fees
+	#if div_value == 0.0:
+	#	continue
+	calc_realized_wins(acc)
+#	for split in acc.splits:
+#		trans = split.transaction
+#		out.write("\t%-30s   vnum %s vdenom %s qnum %s qdenom %s\n" %
+#		          (trans.description, split.value_num, split.value_denom,
+#		          split.quantity_num, split.quantity_denom))
+	out.write("  => %5.2f dividends [%5.2f fees]\n" % (acc.div_value, acc.div_fees))
+out.write("-----------\n")
+out.write("%5.2f EUR dividens [%5.2f dividend fees]" % (gdiv_value, gdiv_fees))
 
 class Transaction(object):
 	def __init__(self):
