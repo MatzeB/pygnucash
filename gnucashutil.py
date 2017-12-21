7f01b89326b770e713e59ce90fd5bb147cf81cb0
Matthias Braun <matze@braunis.de>
2017-12-21 10:44:13 +0100

    No need to display : at begin of account name.path

--- gnucashutil.py
+++ gnucashutil.py
@@ -2,5 +2,7 @@ def full_acc_name(acc, maxdepth=1000):
     if acc.parent is None or maxdepth == 0:
         return ""
     result = full_acc_name(acc.parent, maxdepth-1)
-    result += ":"+acc.name
+    if result != "":
+        result += ":"
+    result += acc.name
     return result
